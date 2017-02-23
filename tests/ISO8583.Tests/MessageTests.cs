7057038005a04cab9f22abfc059238553d4acc02
Robson da Silva <sc.robson@gmail.com>
2017-02-23 11:36:41 -0300

    Adicionando testes unitários básicos

---
+++ tests/ISO8583.Tests/MessageTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
+
+namespace ISO8583.Tests
+{
+    [TestClass]
+    public class MessageTests
+    {
+        private Message CreateMessage()
+        {
+            return new Message("0800") //Message Type
+                .Append(3, "008000") //Process Code
+                .Append(7, "0223141530") //GMT MMddHHmmss
+                .Append(11, "000001") //NSU Terminal
+                .Append(12, "111530") //Local HHmmss
+                .Append(13, "0223") //Local MMdd
+                .Append(32, "1100000000001") //Institute Code (it's LLV so "11" is the size and "00000000001" is the code)
+                .Append(41, "00000001") //Terminal ID
+                .Append(42, "A1B2C3D4E5F6G7H") //Establishment Code
+                .Append(48, "0071270011006") //Additional data
+                .Append(61, "0200SE") //Version
+                .Encode(Encoding.ASCII);
+        }
+
+        [TestMethod]
+        public void Message_ShouldToString()
+        {
+            var expected = "08002238000100C1000800800002231415300000011115300223110000000000100000001A1B2C3D4E5F6G7H00712700110060200SE";
+            var generated = CreateMessage().Build().ToString();
+
+            Assert.AreEqual(expected, generated);
+        }
+
+        [TestMethod]
+        public void Message_ShouldPack()
+        {
+            var expected = 109;
+            var generated = CreateMessage().Build().Pack().Length;
+
+            Assert.AreEqual(expected, generated);
+        }
+    }
+}
---
+++ tests/ISO8583.Tests/UtilTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ISO8583.Tests
+{
+    [TestClass]
+    public class UtilTests
+    {
+        [TestMethod]
+        public void Util_ShouldBuildBinaryBitmap()
+        {
+            var expected = "0010001000111000000000000000000100000000110000010000000000001000";
+            var generated = Util.BuildBinaryBitmap(3, 7, 11, 12, 13, 32, 41, 42, 48, 61);
+
+            Assert.AreEqual(expected, generated);
+        }
+
+        [TestMethod]
+        public void Util_ShouldBuildHexaBitmap()
+        {
+            var expected = "2238000100C10008";
+            var generated = Util.BuildHexaBitmap(3, 7, 11, 12, 13, 32, 41, 42, 48, 61);
+
+            Assert.AreEqual(expected, generated);
+        }
+
+        [TestMethod]
+        public void Util_ShouldConvertBinaryToHexa()
+        {
+            var expected = "2238000100C10008";
+            var generated = Util.ConvertBinaryToHexa("0010001000111000000000000000000100000000110000010000000000001000");
+
+            Assert.AreEqual(expected, generated);
+        }
+    }
+}
